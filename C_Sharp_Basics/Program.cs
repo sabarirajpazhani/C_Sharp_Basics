e7315781a8ff6584142bf819fddf7b69510a6daa
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-23 14:03:20 +0530

    Most commonly used Data Types

--- C_Sharp_Basics/Program.cs
+++ C_Sharp_Basics/Program.cs
@@ -3,13 +3,27 @@
 namespace C_Sharp_Basics {
     public class Program
     {
-        public static void Hello()     //Hello World Console application
-        {
-            Console.WriteLine("Hello World!!");
+        public static void dataTypes() { 
+            int num = 5;
+            char letter = 's';
+            double myNum = 6.99D;
+            float floatNum = 123.0f;
+            bool boolean = true;
+            string str = "Hello Developers";
+
+
+            Console.WriteLine(num);
+            Console.WriteLine(letter);  
+            Console.WriteLine(myNum);
+            Console.WriteLine(floatNum);    
+            Console.WriteLine(boolean);
+            Console.WriteLine(str);
         }
+
         static void Main(String[] args)
         {
-            Hello();
+            dataTypes();
+            
         }
     }
 }
