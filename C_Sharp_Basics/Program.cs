23313ee4894d42f47acacbc09acf66e9e8793551
sabarirajp <sabarirajp@TRNE-SABARIP>
2025-04-23 11:20:10 +0530

    Add project files.

---
+++ C_Sharp_Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_Sharp_Basics {
+    public class Program
+    {
+        public static void Hello()    
+        {
+            Console.WriteLine("Hello World!!");
+        }
+        static void Main(String[] args)
+        {
+            Hello();
+        }
+    }
+}
