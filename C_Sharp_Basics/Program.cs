6c9a826251cdb2b911e2b10d1e6db6d1514a03bf
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-23 14:21:32 +0530

    Varaibles and getting values from user

--- C_Sharp_Basics/Program.cs
+++ C_Sharp_Basics/Program.cs
@@ -20,10 +20,43 @@ namespace C_Sharp_Basics {
             Console.WriteLine(str);
         }
 
+        public static void Variables()
+        {
+            int num = 100;
+            Console.WriteLine(num);
+
+            Console.WriteLine("User Given Input Or getting Input from User");
+            Console.WriteLine("Enter any Integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1);
+
+            Console.WriteLine("Enter Any String: ");
+            string str = Console.ReadLine();    
+            Console.WriteLine(str);
+
+            Console.WriteLine("Enter any float value: ");
+            float f = float.Parse(Console.ReadLine());
+            Console.WriteLine(f);
+
+            Console.WriteLine("Enter any long value: ");
+            long myLong = long.Parse(Console.ReadLine());
+            Console.WriteLine(myLong);
+
+            Console.WriteLine("Enter any Double value: ");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine(d);
+
+            Console.WriteLine("Enter any Boolean value: ");
+            bool boolean = bool.Parse(Console.ReadLine());
+            Console.WriteLine(boolean);
+        }
         static void Main(String[] args)
         {
             dataTypes();
-            
+            Console.WriteLine("--------------------------------");
+            Variables();
+            Console.WriteLine("--------------------------------");
+
         }
     }
 }
